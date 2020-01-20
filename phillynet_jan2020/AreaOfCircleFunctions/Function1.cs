e92974f8361d27a8f3f9742e9c45ed5d2a2b1597
Melusky <mike.melusky@gmail.com>
2020-01-20 18:02:55 -0500

    azure

--- phillynet_jan2020/AreaOfCircleFunctions/Function1.cs
+++ phillynet_jan2020/AreaOfCircleFunctions/Function1.cs
@@ -12,7 +12,7 @@ namespace AreaOfCircleFunctions
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("AreaOfCircle")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
